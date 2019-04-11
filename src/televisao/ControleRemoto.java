3ad9484ab33773cc8c6e723822794c1349e8b9f7
JULIA GRACIELA DO ROSARIO FARIAS <04034625@sempreunama.com.br>
2019-04-11 20:41:10 -0300

    Initial commit

---
+++ src/televisao/ControleRemoto.java
@@ -0,0 +1,16 @@
+package televisao;
+
+public class ControleRemoto {
+    String tv: televisao
+    public static int getConsulta() {
+        return consulta;
+    }
+
+    public static Scanner getEntrada() {
+        return entrada;
+televisao( int som, int canal){
+    this.som=
+    this.canal=
+        }
+    }
+
---
+++ src/televisao/televisao.java
@@ -0,0 +1,18 @@
+package televisao;
+
+public class Televisao {
+    int Som;
+    int Canal;
+    public static int getCanal() {
+        return Canal;
+    }
+    public static void setCanal(int canal) {
+        Televisao.Canal = canal;
+    }
+    public static int getSom() {
+        return som;
+    }
+    public static void setSom(int volume) {
+        Televisao.som = volume;
+    }
+}
